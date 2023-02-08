26c40fa475b729b89587b2d2fac168a8e4c5722b
Fabio Pereira <fabio.pereira@wirsolut.com>
2023-02-08 17:47:31 -0300

    Extraer datos a clase DataSource

--- app/src/main/java/com/example/oficiosejercicio/MainActivity.kt
+++ app/src/main/java/com/example/oficiosejercicio/MainActivity.kt
@@ -6,37 +6,23 @@ import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-open class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
-
-    val jobs: List<DataJobs> = listOf(
-        DataJobs("Cerrajero", "completo"),
-        DataJobs("Carnicero", "completo"),
-        DataJobs("Cervecero", "4 horas"),
-        DataJobs("Remisero", "franquero"),
-        DataJobs("Plomero", "6 horas"),
-        DataJobs("gasista", "completo"),
-        DataJobs("Cerrajero", "completo"),
-        DataJobs("Carnicero", "completo"),
-        DataJobs("Cervecero", "4 horas"),
-        DataJobs("Remisero", "franquero"),
-        DataJobs("Plomero", "6 horas"),
-        DataJobs("gasista", "completo")
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initRecycler()
+
 
         val dataSource = DataSource()
-        val listado = dataSource.listado()
-        //dataSource.listado()
-        // asignar listado a un recylcer view y mostrarlo en pantalla
+        val jobs = dataSource.listado()
+        initRecycler(jobs)
+
+
 
     }
 
-    fun initRecycler() {
+    fun initRecycler(jobs:List<DataJobs>) {
 
         val rvOficios = findViewById<RecyclerView>(R.id.rvJobs)
 
