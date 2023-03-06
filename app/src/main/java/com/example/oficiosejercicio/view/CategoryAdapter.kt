bdd0f7a0543fe9dae5165458e1b46dc68226fd97
Fabio Pereira <fabio.pereira@wirsolut.com>
2023-03-06 17:12:05 -0300

    Se agregó color al adapter, texto mas buscado

--- app/src/main/java/com/example/oficiosejercicio/view/CategoryAdapter.kt
+++ app/src/main/java/com/example/oficiosejercicio/view/CategoryAdapter.kt
@@ -1,12 +1,14 @@
 package com.example.oficiosejercicio.view
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.graphics.toColorInt
 import androidx.recyclerview.widget.RecyclerView
 import com.example.oficiosejercicio.R
 import com.example.oficiosejercicio.model.DataCategory
@@ -31,8 +33,8 @@ class CategoryAdapter(val categoryList: List<DataCategory>) :
 
     class CategoryHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
-        val tvCategory1 = view.findViewById<TextView>(R.id.tvCategory)
 
+        val tvCategory1 = view.findViewById<TextView>(R.id.tvCategory)
 
         fun render(category: DataCategory) {
             tvCategory1.text = category.category
@@ -40,9 +42,8 @@ class CategoryAdapter(val categoryList: List<DataCategory>) :
             val buscado = category.masBuscado
 
             if (buscado) {
-                tvCategory1.text ="Categoria más buscada hoy -->  " + category.category
-
-
+                tvCategory1.text = "Categoria más buscada hoy es: " + category.category
+                tvCategory1.setTextColor(Color.parseColor("#05BBF3"))
 
 
             }
